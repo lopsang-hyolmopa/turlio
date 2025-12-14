95f393a8fb21ea1fc35398f70ad979c20f289ad5
Lopsang Lama <lopsang.hyolmopa@gmail.com>
2025-12-14 22:15:40 +0545

    add url and user model and schema

---
+++ backend/src/models/url.model.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+const urlSchema = new Schema(
+  {
+    title: {
+      type: String,
+      index: true,
+    },
+    originalUrl: {
+      type: String,
+      required: true,
+    },
+    shortCode: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    clicks: {
+      type: Number,
+      default: 0,
+    },
+    uesr: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Url = new mongoose.Model("Url", urlSchema);
---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = new mongoose.Model("User", userSchema);
