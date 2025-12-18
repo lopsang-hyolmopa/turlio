c5d079410eb368ad530134d0a5d7380c1b7bbc80
Lopsang Lama <lopsang.hyolmopa@gmail.com>
2025-12-18 16:42:57 +0545

    add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { json, urlencoded } from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import urlRoutes from "./routes/url.route.js";
@@ -7,8 +8,16 @@ import userRoutes from "./routes/user.route.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import { asyncHandler } from "./utils/asyncHandler.js";
 import { redirectFromShortUrl } from "./controllers/url.controller.js";
+import { APP_URL } from "./constants.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: APP_URL,
+    credentials: true,
+  })
+);
 
 app.use(json());
 app.use(urlencoded({ extended: true, limit: "4kb" }));
--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,3 +1,4 @@
 export const DB_NAME = "turlio";
 export const SERVER_PORT = process.env.PORT || 8001;
 export const SERVER_URL = `http://localhost:${SERVER_PORT}`;
+export const APP_URL = "http://localhost:5173";
